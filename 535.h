31ab9f943e330d4cf0c9d390d8a24895c762f62a
ZhongJ <280673335@qq.com>
2018-11-16 09:48:12 +0800

    535 Encode and Decode TinyURL

---
+++ 535.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+	
+public:
+	//数据太无语了 直接返回就好 
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {        
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+		return shortUrl;     
+    }
+};
